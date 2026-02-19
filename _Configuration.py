1c33462b3c1fffc40f771bd285fe655862851900
premgyani_byte <premgyani@gmail.com>
2026-02-19 17:47:29 +0000

    API Key was corrected

--- _Configuration.py
+++ _Configuration.py
@@ -16,7 +16,7 @@ class AppConfig(BaseModel):
     # =================================================================
     # !!! SENSITIVE DATA - NOW WITH TYPE HINTS !!!
     # =================================================================
-    AI_KEY: str = "AIzaSyATQLxxxxxxxxxxxxxxxxxxxxxxxx" # Added : str
+    AI_KEY: str = "AIzaSyATQLT_eYrQyd5i_YF0nQs-HQT39rW7vTM" # Added : str
     SS_ID: str = "17qAoBsYn11CkZohNR6aSpu0Rb_JvrETRrIusgHt3AGU" # Added : str
     PREM_EMAIL: str = "premgyani@gmail.com" # Added : str
     LOCK_PASSWORD: str = "PASSWORD" # Added : str
